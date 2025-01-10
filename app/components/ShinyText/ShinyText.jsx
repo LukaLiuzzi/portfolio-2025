3a9b2d504e2d87cbb0220532cf95de5fc2aca787
Luka Liuzzi <lukaliuzzidev@gmail.com>
2025-01-10 02:43:56 -0300

    fix shainy colors

--- app/components/ShinyText/ShinyText.jsx
+++ app/components/ShinyText/ShinyText.jsx
@@ -1,14 +1,27 @@
+"use client"
+
+import { useState, useEffect } from "react"
+
 const ShinyText = ({ text, disabled = false, speed = 5, className = "" }) => {
   const animationDuration = `${speed}s`
+  const [isDark, setIsDark] = useState(false) // Default value
+
+  useEffect(() => {
+    const darkMode = localStorage.getItem("darkMode") === "true"
+    setIsDark(darkMode)
+  }, [])
 
   return (
     <div
-      className={`text-muted-foreground bg-clip-text inline-block ${
+      className={`dark:text-[#b5b5b5a4] text-[#535252a4] bg-clip-text inline-block ${
         disabled ? "" : "animate-shine"
       } ${className}`}
       style={{
-        backgroundImage:
-          "linear-gradient(120deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0) 60%)",
+        backgroundImage: `${
+          isDark
+            ? "linear-gradient(120deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0) 60%)"
+            : "linear-gradient(120deg, rgba(0,0,0, 0) 40%, rgba(0,0,0, 0.8) 50%, rgba(0,0,0, 0) 60%)"
+        }`,
         backgroundSize: "200% 100%",
         WebkitBackgroundClip: "text",
         animationDuration: animationDuration,
